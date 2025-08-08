62fe3fdcd65d5fbf119e90c75b9a75c7681e7e88
Igor Marinho <igor.siqueira700@gmail.com>
2025-08-08 15:24:21 -0300

    plano b

---
+++ run_worker.bat
@@ -0,0 +1,7 @@
+set ZK="C:\tmp\apache-zookeeper-3.9.3-bin"
+set CP_ZK=.;%ZK%\lib\zookeeper-3.9.3.jar;%ZK%\lib\zookeeper-jute-3.9.3.jar;%ZK%\lib\slf4j-api-1.7.30.jar;%ZK%\lib\logback-core-1.2.13.jar;%ZK%\lib\logback-classic-1.2.13.jar;%ZK%\lib\netty-handler-4.1.113.Final.jar
+javac -cp %CP_ZK% *.java
+@echo ** Iniciando Worker **
+set BARRIER_SIZE=3
+@echo Tamanho do grupo na barreira = %BARRIER_SIZE%
+java -cp %CP_ZK% -Dlogback.configurationFile=file:%ZK%\conf\logback.xml SyncPrimitive worker localhost %BARRIER_SIZE%
