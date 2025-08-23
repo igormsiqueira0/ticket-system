77d19fbce93b028efac252b3d115f16a3ea0d01e
Igor Marinho <igor.siqueira700@gmail.com>
2025-08-23 14:36:33 -0300

    Delete LeMans directory

--- LeMans/run_racer.bat
+++
@@ -1,7 +0,0 @@
-set ZK="C:\tmp\apache-zookeeper-3.9.3-bin"
-set CP_ZK=.;%ZK%\lib\zookeeper-3.9.3.jar;%ZK%\lib\zookeeper-jute-3.9.3.jar;%ZK%\lib\slf4j-api-1.7.30.jar;%ZK%\lib\logback-core-1.2.13.jar;%ZK%\lib\logback-classic-1.2.13.jar;%ZK%\lib\netty-handler-4.1.113.Final.jar
-javac -cp %CP_ZK% *.java
-@echo ** Iniciando Corredor **
-set RACERS=3
-@echo Numero de corredores no grid de largada = %RACER_COUNT%
-java -cp %CP_ZK% -Dlogback.configurationFile=file:%ZK%\conf\logback.xml LeMansRace racer localhost %RACERS%
--- LeMans/run_timer.bat
+++
@@ -1,6 +0,0 @@
-set ZK="C:\tmp\apache-zookeeper-3.9.3-bin"
-set CP_ZK=.;%ZK%\lib\zookeeper-3.9.3.jar;%ZK%\lib\zookeeper-jute-3.9.3.jar;%ZK%\lib\slf4j-api-1.7.30.jar;%ZK%\lib\logback-core-1.2.13.jar;%ZK%\lib\logback-classic-1.2.13.jar;%ZK%\lib\netty-handler-4.1.113.Final.jar
-javac -cp %CP_ZK% *.java
-@echo ***** Cronômetro Le Mans - Produtor de Tempo *****
-set TEMPOS=24
-java -cp %CP_ZK% -Dlogback.configurationFile=file:%ZK%\conf\logback.xml LeMansRace timeProducer localhost %TEMPOS%
